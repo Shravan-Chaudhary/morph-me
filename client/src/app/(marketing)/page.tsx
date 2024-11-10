fadf79ad7366a44003dbe80f714013d683320809
Shravan <shravansinghchaudhary@gmail.com>
2024-11-10 14:31:45 +0530

    feat(client): :sparkles: Add pricing cards section to home page

--- client/src/app/(marketing)/page.tsx
+++ client/src/app/(marketing)/page.tsx
@@ -3,9 +3,18 @@ import { AnimatedShinyPill } from '@/components/ui/animated-shiny-pill'
 import AnimatedShinyText from '@/components/ui/animated-shiny-text'
 import { BorderBeam } from '@/components/ui/border-beam'
 import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import SectionBadge from '@/components/ui/section-badge'
-import { features, process } from '@/constants'
-import { ArrowRight, ChevronRight } from 'lucide-react'
+import { features, pricingCards, process } from '@/constants'
+import { cn } from '@/lib/utils'
+import { ArrowRight, ChevronRight, Zap } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -134,7 +143,7 @@ const HomePage = () => {
                 >
                   <div className='bg-gray-50 w-16 h-16 rounded-full flex items-center justify-center mb-4'>
                     {feature.icon && (
-                      <feature.icon className='w-8 h-8 text-neutral-800' />
+                      <feature.icon className='w-8 h-8 text-primary' />
                     )}
                   </div>
                   <h3 className='text-lg font-semibold mb-2'>
@@ -164,6 +173,56 @@ const HomePage = () => {
           </div>
         </Container>
         <Container className='flex items-center justify-center'>
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 w-full md:gap-8 py-10 md:py-20 flex-wrap max-w-4xl'>
+            {pricingCards.map((card) => (
+              <Card
+                key={card.title}
+                className={cn(
+                  'flex flex-col w-full border-x-neutral-700',
+                  card.title == 'Unlimited Saas' && 'border-2 border-primary',
+                )}
+              >
+                <CardHeader className='border-2 border-border'>
+                  <span>{card.title}</span>
+                  <CardTitle
+                    className={cn(
+                      card.title !== 'Unlimited Saas' &&
+                        'text-muted-foreground',
+                    )}
+                  >
+                    {card.price}
+                  </CardTitle>
+                  <CardDescription>{card.description}</CardDescription>
+                </CardHeader>
+                <CardContent className='pt-6 space-y-3'>
+                  {card.features.map((feature) => (
+                    <div key={feature} className='flex items-center gap-2'>
+                      <Zap className='w-4 h-4 fill-primary text-primary' />
+                      <p className=''>{feature}</p>
+                    </div>
+                  ))}
+                </CardContent>
+                <CardFooter className='mt-auto w-full'>
+                  <Link
+                    href='#'
+                    className={cn(
+                      'w-full text-center text-primary-foreground bg-primary p-2 rounded-md text-sm font-medium',
+                      card.title !== 'Unlimited Saas' &&
+                        '!bg-foreground !text-background',
+                    )}
+                  >
+                    {card.buttonText}
+                  </Link>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        </Container>
+      </Wrapper>
+
+      {/* Testimonials */}
+      <Wrapper className='max-w-6xl flex flex-col items-center justify-center py-12 relative'>
+        <Container>
           <div className=''></div>
         </Container>
       </Wrapper>
