31713c93a895cdf8863c1f2a7533305b0a35e5ad
Shravan <shravansinghchaudhary@gmail.com>
2024-11-11 11:25:02 +0530

    feat(client): :necktie: Add metadata to root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,10 @@
+import { SITE_CONFIG } from '@/config'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 
 const font = Inter({ subsets: ['latin'] })
+export const metadata = SITE_CONFIG
 
 export default function RootLayout({
   children,
