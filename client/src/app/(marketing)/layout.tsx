c08995a3f31581401a42d9d11029f4f342120fba
Shravan <shravansinghchaudhary@gmail.com>
2024-11-11 11:23:33 +0530

    feat(client): Add footer component to marketing layout

--- client/src/app/(marketing)/layout.tsx
+++ client/src/app/(marketing)/layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from '@/components'
+import { Footer, Navbar } from '@/components'
 import React from 'react'
 
 interface Props {
@@ -10,6 +10,7 @@ const MarketingLayout = ({ children }: Props) => {
     <div className='flex flex-col items-center w-full'>
       <Navbar />
       {children}
+      <Footer />
     </div>
   )
 }
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,5 +2,7 @@ import Icons from './global/icons'
 import Navbar from './navigation/navbar'
 import Wrapper from './global/wrapper'
 import Container from './global/container'
+import SpellcastingLoader from './global/spell-casting-loader'
+import Footer from './navigation/footer'
 
-export { Icons, Navbar, Wrapper, Container }
+export { Icons, Navbar, Wrapper, Container, SpellcastingLoader, Footer }
