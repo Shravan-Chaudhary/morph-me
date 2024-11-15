42ca21e2be70e9d9a83ded37637c921db170132e
Shravan <shravansinghchaudhary@gmail.com>
2024-11-15 16:25:30 +0530

    feat(server): :necktie: Add jwt parsing utility function

--- server/internal/auth/jwt.go
+++ server/internal/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"morph-me/internal/http/model"
 	"time"
 
@@ -11,6 +12,7 @@ func SignJWT(jwtSecret string, user *model.User) (string, error) {
 	const (
 		THIRTY_DAYS = 30 * 24 * time.Hour
 	)
+
 	claims := jwt.MapClaims{
 		"sub":       user.ID.Hex(),
 		"name":      user.Name,
@@ -21,9 +23,35 @@ func SignJWT(jwtSecret string, user *model.User) (string, error) {
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	signedToken, err := token.SignedString([]byte("topSecretString"))
+	signedToken, err := token.SignedString([]byte(jwtSecret))
 	if err != nil {
 		return "", err
 	}
 	return signedToken, nil
+}
+
+// ParseJWT parses and validates the JWT token
+func ParseJWT(tokenString string, jwtSecret string) (string, error) {
+	// Parse the token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+		// Check if the token is valid and extract claims
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		email, ok := claims["email"].(string)
+		if !ok {
+			return "", errors.New("email claim not found")
+		}
+		return email, nil
+	}
+
+	return "", errors.New("invalid token")
 }
