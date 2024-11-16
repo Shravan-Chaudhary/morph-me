34e6da29ee0468c891982c0bc6251c1d0211b84b
Shravan <shravansinghchaudhary@gmail.com>
2024-11-16 13:33:01 +0530

    fix(client): :zap: Fix flickering of footer on the morph page

--- client/src/app/morph/layout.tsx
+++ client/src/app/morph/layout.tsx
@@ -1,11 +1,17 @@
 import { Footer, Navbar } from '@/components'
 import React from 'react'
+import { redirect } from 'next/navigation'
+import getSession from '@/lib/session'
 
 interface Props {
   children: React.ReactNode
 }
 
-const MorphLayout = ({ children }: Props) => {
+const MorphLayout = async ({ children }: Props) => {
+  const session = await getSession()
+  if (!session) {
+    redirect('/sign-in')
+  }
   return (
     <div className='flex flex-col items-center w-full'>
       <Navbar />
--- client/src/app/morph/page.tsx
+++ client/src/app/morph/page.tsx
@@ -1,12 +1,6 @@
-import getSession from '@/lib/session'
 import MorphForm from './components/morph-form'
-import { redirect } from 'next/navigation'
 
 const MorphPage = async () => {
-  const session = await getSession()
-  if (!session) {
-    redirect('/sign-in')
-  }
   return (
     <>
       <MorphForm />
