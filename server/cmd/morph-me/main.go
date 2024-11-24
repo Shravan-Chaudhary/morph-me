d34383d817dbbae2848c7ff4d00f48530372eefd
Shravan <shravansinghchaudhary@gmail.com>
2024-11-25 03:41:12 +0530

    refactor(server): :art: Refactor prediction handlers to separate files

--- server/cmd/morph-me/main.go
+++ server/cmd/morph-me/main.go
@@ -2,28 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"log"
 	"log/slog"
 	"morph-me/internal/auth"
 	"morph-me/internal/database"
 	"morph-me/internal/http/handler"
-	"morph-me/internal/http/model"
 	"morph-me/internal/http/respository"
 	"morph-me/internal/http/service"
 	storage "morph-me/internal/object-storage"
 	"morph-me/internal/server"
 	"morph-me/internal/util"
-	"net/http"
 	"os"
 	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/replicate/replicate-go"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -82,34 +75,7 @@ func main() {
 
 	// Router
 	router := gin.Default()
-	router.Use(func(c *gin.Context) {
-		slog.Debug("Request headers",
-			"origin", c.GetHeader("Origin"),
-			"cookie", c.GetHeader("Cookie"),
-		)
-		c.Next()
-	})
-	corsconfig := cors.DefaultConfig()
-	corsconfig.AllowOrigins = []string{config.CLIENT_URL, "https://morph-me.vercel.app", "http://localhost:3000"}
-	corsconfig.AllowMethods = []string{"GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"}
-	corsconfig.AllowHeaders = []string{
-		"Origin",
-		"Content-Type",
-		"Accept",
-		"Authorization",
-		"Cross-Origin-Opener-Policy",
-		"Cross-Origin-Embedder-Policy",
-		"Cookie",
-		"Set-Cookie",
-	}
-	corsconfig.ExposeHeaders = []string{
-		"Cross-Origin-Opener-Policy",
-		"Cross-Origin-Embedder-Policy",
-		"Set-Cookie",
-	}
-	corsconfig.AllowCredentials = true
-	corsconfig.MaxAge = 24 * time.Hour
-	router.Use(cors.New(corsconfig))
+	router.Use(util.SetupCORS(config))
 
 	// Instances
 	userRespository, err := respository.NewMongoUserRepository(client, config.DATABASE_NAME)
@@ -125,6 +91,7 @@ func main() {
 	authMiddleware := auth.AuthMiddleware(config.JWT_SECRET)
 	userService := service.NewUserService(userRespository)
 	userHandler := handler.NewUserHandler(userService)
+	predicitonHandler := handler.NewPredictionHandler(r8, creditRepository, &config)
 	googleHandler := handler.NewGoogleOAuthHandler(userService, googleOauthConfig, config.JWT_SECRET)
 	uploadHandler := handler.NewUploadHandler(s3Storage)
 
@@ -133,142 +100,12 @@ func main() {
 	router.POST("/users", userHandler.HandleCreateUser)
 	router.POST("/upload", uploadHandler.Upload)
 	router.GET("/self", authMiddleware, userHandler.Self)
-	router.GET("/predictions/:prediction_id", authMiddleware, func(c *gin.Context) {
-		GetPredictionStatusHandler(c, &config)
-	})
-	router.POST("/predictions", authMiddleware, func(c *gin.Context) {
-		PredictionHandler(c, r8, creditRepository)
-	})
+	router.POST("/predictions", authMiddleware, predicitonHandler.PredictionHandler)
+	router.GET("/predictions/:prediction_id", authMiddleware, predicitonHandler.GetPredictionStatusHandler)
 	router.GET("/remaining-credits", authMiddleware, userHandler.RemainingCredits)
 
 	s := server.NewServer(":8080", router)
 	s.Start()
-}
-
-type ProcessRequest struct {
-	ImageUrl string `json:"image_url" binding:"required"`
-	Style    string `json:"style" binding:"required"`
-}
-
-// PredictionHandler handler
-func PredictionHandler(c *gin.Context, r8 *replicate.Client, creditRepo *respository.CreditRepository) {
-	// Get UserID
-	userIDStr := c.MustGet("userID").(string)
-	userID, err := primitive.ObjectIDFromHex(userIDStr)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error(), "status": "error"})
-		return
-	}
-
-	// MongodB Transaction
-	session, err := creditRepo.Client.StartSession()
-	if err != nil {
-		fmt.Println("session creation", err)
-		c.JSON(500, gin.H{"error": "Internal server error", "status": "error"})
-		return
-	}
-	defer session.EndSession(c.Request.Context())
-
-	var prediction *replicate.Prediction
-	var tx *model.Transaction
-
-	// The whole show wrapped in withTransaction
-	_, err = session.WithTransaction(c.Request.Context(), func(sessCtx mongo.SessionContext) (interface{}, error) {
-		// 1.Create first paper trail
-		tx = &model.Transaction{
-			ID:        primitive.NewObjectID(),
-			UserID:    userID,
-			Type:      "DEDUCT",
-			Amount:    1,
-			Status:    "PENDING",
-			CreatedAt: time.Now(),
-		}
-		if err := creditRepo.CreateTransaction(sessCtx, tx); err != nil {
-			fmt.Println("transaction creation", err)
-			return nil, fmt.Errorf("failed to create Transaction: %v", err)
-		}
-
-		// 2. Yoink the credit (atomically)
-		if err := creditRepo.DeductUserCredit(sessCtx, userID); err != nil {
-			fmt.Println("deducting credit", err)
-			return nil, fmt.Errorf("insufficient credits")
-		}
-
-		// 3. Create the prediction
-		var req ProcessRequest
-		if err := c.ShouldBindJSON(&req); err != nil {
-			fmt.Println("binding request", err)
-			return nil, err
-		}
-
-		input := replicate.PredictionInput{
-			"image":               req.ImageUrl,
-			"style":               req.Style,
-			"prompt":              "a person",
-			"negative_prompt":     "boring",
-			"prompt_strength":     4.5,
-			"denoising_strength":  1,
-			"instant_id_strength": 0.8,
-		}
-		// Run the model and wait for output
-		version := "a07f252abbbd832009640b27f063ea52d87d7a23a185ca165bec23b5adc8deaf"
-		prediction, err = r8.CreatePrediction(c.Request.Context(), version, input, nil, false) // nil webhook since we're waiting
-		if err != nil {
-			fmt.Println("creating prediction", err)
-			return nil, fmt.Errorf("failed to create prediction: %v", err)
-		}
-		fmt.Println(prediction.ID)
-
-		// 4. Update paper trail
-		if err = creditRepo.UpdateTransactionStatus(sessCtx, tx.ID, "COMPLETED", prediction.ID); err != nil {
-			fmt.Println("updating transaction status", err)
-			return nil, fmt.Errorf("failed to update transaction status: %v", err)
-		}
-		return prediction.ID, nil
-	})
-
-	if err != nil {
-		if tx != nil {
-			creditRepo.UpdateTransactionStatus(c.Request.Context(), tx.ID, "FAILED", "")
-		}
-		fmt.Println("transaction error", err)
-		c.JSON(500, gin.H{"status": "error", "error": err.Error()})
-		return
-	}
-
-	// Return the processed image URLs
-	c.JSON(200, gin.H{
-		"status":        "success",
-		"prediction_id": prediction.ID,
-	})
-}
-
-// GetPredictionStatusHandler handler
-func GetPredictionStatusHandler(c *gin.Context, config *util.Config) {
-	url := "https://api.replicate.com/v1/predictions/" + c.Param("prediction_id")
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
-		return
-	}
-	req.Header.Set("Authorization", "Token "+config.REPLICATE_TOKEN)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
-		return
-	}
-
-	c.Writer.Header().Set("Content-Type", "application/json")
-	c.Writer.WriteHeader(resp.StatusCode)
-	c.Writer.Write(body)
 }
 
 // 1. Check for file type
@@ -280,10 +117,11 @@ func GetPredictionStatusHandler(c *gin.Context, config *util.Config) {
 // 7. Check for secrets in code replace with env vars
 
 // Frontend
-// 2. Frontend check for credits (if credits < 1, disable button)
+// 1. Type of photo to upload
+// 2. Example prompts
 // 3. Check for file type before uploading
+// 4. Wand animation and scroll down on mobile devices while loading on transfrom
+// 5. Share and download button
 
 // Production
-// 1. Google OAuth from test to production
-// 2. Gin Mode to release
 // 3. Production Database
---
+++ server/internal/http/handler/prediction.go
@@ -0,0 +1,183 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"morph-me/internal/http/model"
+	"morph-me/internal/http/respository"
+	"morph-me/internal/util"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/replicate/replicate-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ProcessRequest struct {
+	ImageUrl string `json:"image_url" binding:"required"`
+	Style    string `json:"style" binding:"required"`
+}
+
+type PredictionHandler struct {
+	r8         *replicate.Client
+	creditRepo *respository.CreditRepository
+	config     *util.Config
+}
+
+func NewPredictionHandler(r8 *replicate.Client, creditRepo *respository.CreditRepository, config *util.Config) *PredictionHandler {
+	return &PredictionHandler{
+		r8:         r8,
+		creditRepo: creditRepo,
+		config:     config,
+	}
+}
+
+func (h *PredictionHandler) PredictionHandler(c *gin.Context) {
+	// Get UserID
+	userIDStr := c.MustGet("userID").(string)
+	userID, err := primitive.ObjectIDFromHex(userIDStr)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+
+	// MongodB Transaction
+	session, err := h.creditRepo.Client.StartSession()
+	if err != nil {
+		fmt.Println("session creation", err)
+		c.JSON(500, gin.H{"error": "Internal server error", "status": "error"})
+		return
+	}
+	defer session.EndSession(c.Request.Context())
+
+	var prediction *replicate.Prediction
+	var tx *model.Transaction
+
+	// The whole show wrapped in withTransaction
+	_, err = session.WithTransaction(c.Request.Context(), func(sessCtx mongo.SessionContext) (interface{}, error) {
+		// 1.Create first paper trail
+		tx = &model.Transaction{
+			ID:        primitive.NewObjectID(),
+			UserID:    userID,
+			Type:      "DEDUCT",
+			Amount:    1,
+			Status:    "PENDING",
+			CreatedAt: time.Now(),
+		}
+		if err := h.creditRepo.CreateTransaction(sessCtx, tx); err != nil {
+			fmt.Println("transaction creation", err)
+			return nil, fmt.Errorf("failed to create Transaction: %v", err)
+		}
+
+		// 2. Yoink the credit (atomically)
+		if err := h.creditRepo.DeductUserCredit(sessCtx, userID); err != nil {
+			fmt.Println("deducting credit", err)
+			return nil, fmt.Errorf("insufficient credits")
+		}
+
+		// 3. Create the prediction
+		var req ProcessRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			fmt.Println("binding request", err)
+			return nil, err
+		}
+
+		input := replicate.PredictionInput{
+			"image":               req.ImageUrl,
+			"style":               req.Style,
+			"prompt":              "a person",
+			"negative_prompt":     "boring",
+			"prompt_strength":     4.5,
+			"denoising_strength":  1,
+			"instant_id_strength": 0.8,
+		}
+		// Run the model and wait for output
+		version := "a07f252abbbd832009640b27f063ea52d87d7a23a185ca165bec23b5adc8deaf"
+		prediction, err = h.r8.CreatePrediction(c.Request.Context(), version, input, nil, false) // nil webhook since we're waiting
+		if err != nil {
+			fmt.Println("creating prediction", err)
+			return nil, fmt.Errorf("failed to create prediction: %v", err)
+		}
+		fmt.Println(prediction.ID)
+
+		// 4. Update paper trail
+		if err = h.creditRepo.UpdateTransactionStatus(sessCtx, tx.ID, "COMPLETED", prediction.ID); err != nil {
+			fmt.Println("updating transaction status", err)
+			return nil, fmt.Errorf("failed to update transaction status: %v", err)
+		}
+		return prediction.ID, nil
+	})
+
+	if err != nil {
+		if tx != nil {
+			h.creditRepo.UpdateTransactionStatus(c.Request.Context(), tx.ID, "FAILED", "")
+		}
+		fmt.Println("transaction error", err)
+		c.JSON(500, gin.H{"status": "error", "error": err.Error()})
+		return
+	}
+
+	// Return the processed image URLs
+	c.JSON(200, gin.H{
+		"status":        "success",
+		"prediction_id": prediction.ID,
+	})
+}
+
+// GetPredictionStatusHandler handler
+func GetPredictionStatusHandler(c *gin.Context, config *util.Config) {
+	url := "https://api.replicate.com/v1/predictions/" + c.Param("prediction_id")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+	req.Header.Set("Authorization", "Token "+config.REPLICATE_TOKEN)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(resp.StatusCode)
+	c.Writer.Write(body)
+}
+
+func (h *PredictionHandler) GetPredictionStatusHandler(c *gin.Context) {
+	url := "https://api.replicate.com/v1/predictions/" + c.Param("prediction_id")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+	req.Header.Set("Authorization", "Token "+h.config.REPLICATE_TOKEN)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "status": "error"})
+		return
+	}
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(resp.StatusCode)
+	c.Writer.Write(body)
+
+}
