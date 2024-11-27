793e680c3ec4a73c678368fc2934f28b63757c8d
Shravan <shravansinghchaudhary@gmail.com>
2024-11-27 09:43:21 +0530

    refactor(client): Refactor buy credits page layout

--- client/src/app/buy-credits/page.tsx
+++ client/src/app/buy-credits/page.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button'
 import CreditDisplay from './components/credit-display'
 import CustomSlider from './components/custom-slider'
 import { useUserStore } from '@/stores/user-store'
+import { Container, Wrapper } from '@/components'
 
 export default function PricingPage() {
   const [price, setPrice] = useState(1)
@@ -20,41 +21,46 @@ export default function PricingPage() {
   }, [price])
 
   return (
-    <div className='min-h-screen bg-gradient-to-b from-primary/10 to-background flex flex-col items-center justify-center p-4 w-full'>
-      <div className='flex items-center justify-center flex-col mb-8'>
-        <h1 className='text-3xl md:text-5xl font-bold text-neutral-800 '>
-          Buy <span className='text-primary'>MorphMe</span> Credits
-        </h1>
-        <p className='text-base md:text-lg text-muted-foreground font-medium mt-5'>
-          You have <span className='text-primary'>{user?.credits} credits</span>
-          . Join hundereds of morphers by buying more below
-        </p>
-      </div>
-      <Card className='w-full max-w-2xl'>
-        <CardContent className='pt-6'>
-          <CreditDisplay credits={credits} />
-          <div className='text-center mb-6'>
-            <p className='text-3xl font-semibold text-foreground'>
-              ${price.toFixed(2)}
+    <Wrapper>
+      <Container>
+        <div className='min-h-screen flex flex-col items-center justify-center p-4 w-full'>
+          <div className='flex items-center justify-center flex-col mb-8'>
+            <h1 className='text-3xl md:text-5xl font-bold text-neutral-800 '>
+              Buy <span className='text-primary'>MorphMe</span> Credits
+            </h1>
+            <p className='text-base md:text-lg text-muted-foreground font-medium mt-5'>
+              You have{' '}
+              <span className='text-primary'>{user?.credits} credits</span>.
+              Join hundereds of morphers by buying more below
             </p>
           </div>
-          <CustomSlider
-            min={1}
-            max={20}
-            step={0.5}
-            value={price}
-            onChange={(value) => setPrice(value)}
-          />
-        </CardContent>
-        <CardFooter className='flex justify-center'>
-          <Button
-            size='lg'
-            className='rounded-full transition-all duration-200'
-          >
-            Buy Now
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+          <Card className='w-full max-w-2xl'>
+            <CardContent className='pt-6'>
+              <CreditDisplay credits={credits} />
+              <div className='text-center mb-6'>
+                <p className='text-3xl font-semibold text-foreground'>
+                  ${price.toFixed(2)}
+                </p>
+              </div>
+              <CustomSlider
+                min={1}
+                max={20}
+                step={0.5}
+                value={price}
+                onChange={(value) => setPrice(value)}
+              />
+            </CardContent>
+            <CardFooter className='flex justify-center'>
+              <Button
+                size='lg'
+                className='rounded-full transition-all duration-200'
+              >
+                Buy Now
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </Container>
+    </Wrapper>
   )
 }
